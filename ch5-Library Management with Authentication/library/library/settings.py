16511ba7a8dbb752a156bb88f8a981890809105e
srujan <srujangowda762@gmail.com>
2021-06-25 23:13:32 +0530

    hello world

--- ch5-Library Management with Authentication/library/library/settings.py
+++ ch5-Library Management with Authentication/library/library/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'library', 
-        'USER': 'postgres', 
-        'PASSWORD': 'hello',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
